5bfe5fec1d99065348da1fc98069ebe0ffcbef9d
nastiashpyt <nastiashpyt>
2022-11-03 16:44:18 +0000

    Added DTL class

--- BoredAPI/src/test/java/com/sparta/boredAPI/BoredAPITests.java
+++ BoredAPI/src/test/java/com/sparta/boredAPI/BoredAPITests.java
@@ -1,4 +1,5 @@
 package com.sparta.boredAPI;
 
 public class BoredAPITests {
+
 }
---
+++ BoredAPI/src/test/java/com/sparta/boredAPI/dtl/Response.java
@@ -0,0 +1,55 @@
+package com.sparta.boredAPI.dtl;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Response{
+
+	@JsonProperty("activity")
+	private String activity;
+
+	@JsonProperty("accessibility")
+	private Integer accessibility;
+
+	@JsonProperty("price")
+	private Integer price;
+
+	@JsonProperty("link")
+	private String link;
+
+	@JsonProperty("type")
+	private String type;
+
+	@JsonProperty("key")
+	private String key;
+
+	@JsonProperty("participants")
+	private Integer participants;
+
+	public String getActivity(){
+		return activity;
+	}
+
+	public Integer getAccessibility(){
+		return accessibility;
+	}
+
+	public Integer getPrice(){
+		return price;
+	}
+
+	public String getLink(){
+		return link;
+	}
+
+	public String getType(){
+		return type;
+	}
+
+	public String getKey(){
+		return key;
+	}
+
+	public Integer getParticipants(){
+		return participants;
+	}
+}
