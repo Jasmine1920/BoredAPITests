99b64c14937972efe07e7c7f42d9b6782c3d9c24
nastiashpyt <nastiashpyt>
2022-11-04 09:51:08 +0000

    Added tests to PriceOutOfRangeTests

--- BoredAPI/src/test/java/com/sparta/boredAPI/PriceOutOfRangeTests.java
+++ BoredAPI/src/test/java/com/sparta/boredAPI/PriceOutOfRangeTests.java
@@ -1,9 +1,8 @@
 package com.sparta.boredAPI;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.sparta.boredAPI.dto.Response;
+import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
@@ -15,15 +14,15 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
-public class RandomActivityTests {
+public class PriceOutOfRangeTests {
     private static HttpResponse<String> httpResponse = null;
-    private static Response response = null;
+    private static JSONObject jsonObject = null;
 
     @BeforeAll
     public static void oneTimeSetUp() {
         HttpClient httpClient = HttpClient.newBuilder().build();
         HttpRequest httpRequest = HttpRequest.newBuilder()
-                .uri(URI.create("https://www.boredapi.com/api/activity/"))
+                .uri(URI.create("https://www.boredapi.com/api/activity?price=2"))
                 .setHeader("Content-type", "application/json")
                 .build();
 
@@ -33,11 +32,10 @@ public class RandomActivityTests {
             e.printStackTrace();
         }
 
+        JSONParser jsonParser = new JSONParser();
         try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            response = objectMapper.readValue(httpResponse.body(), Response.class);
-        }
-        catch (JsonProcessingException e) {
+            jsonObject = (JSONObject) jsonParser.parse(httpResponse.body());
+        } catch (ParseException e) {
             e.printStackTrace();
         }
     }
@@ -45,25 +43,24 @@ public class RandomActivityTests {
     @Test
     @DisplayName("URI Path")
     public void testUriPath() {
-        Assertions.assertEquals("/api/activity/", httpResponse.uri().getPath());
+        Assertions.assertEquals("/api/activity", httpResponse.uri().getPath());
     }
 
     @Test
     @DisplayName("Full URI")
     public void testFullUri() {
-        Assertions.assertEquals("https://www.boredapi.com/api/activity/", httpResponse.uri().toString());
+        Assertions.assertEquals("https://www.boredapi.com/api/activity?price=2", httpResponse.uri().toString());
     }
 
     @Test
     @DisplayName("Status code is 200")
     public void testStatusCode() {
-        Assertions.assertEquals(200, httpResponse.statusCode());
+        Assertions.assertEquals(200L, httpResponse.statusCode());
     }
 
     @Test
-    @DisplayName("Response object is not empty")
-    public void testResponseObjNotEmpty() {
-        Assertions.assertTrue(response.isNotEmpty());
+    @DisplayName("Error message")
+    public void testResult() {
+        Assertions.assertEquals("No activity found with the specified parameters", jsonObject.get("error"));
     }
-
 }
