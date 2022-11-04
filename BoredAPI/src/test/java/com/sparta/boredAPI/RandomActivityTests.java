7c99974677abba5f67d46109c6f473da37c51d69
nastiashpyt <nastiashpyt>
2022-11-04 14:00:04 +0000

    Deleted dead code

--- BoredAPI/src/test/java/com/sparta/boredAPI/RandomActivityTests.java
+++ BoredAPI/src/test/java/com/sparta/boredAPI/RandomActivityTests.java
@@ -3,7 +3,6 @@ package com.sparta.boredAPI;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sparta.boredAPI.dto.Response;
-import org.json.simple.parser.JSONParser;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
