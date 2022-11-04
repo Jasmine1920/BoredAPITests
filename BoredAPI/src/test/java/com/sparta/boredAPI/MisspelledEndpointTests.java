b8c474432694f11a54d49aa6e8586fd8cfcf2a12
nastiashpyt <nastiashpyt>
2022-11-04 13:41:23 +0000

    Added tests to MisspelledEndpointTests

---
+++ BoredAPI/src/test/java/com/sparta/boredAPI/MisspelledEndpointTests.java
@@ -0,0 +1,66 @@
+package com.sparta.boredAPI;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class MisspelledEndpointTests {
+    private static HttpResponse<String> httpResponse = null;
+    private static JSONObject jsonObject = null;
+
+    @BeforeAll
+    public static void oneTimeSetUp() {
+        HttpClient httpClient = HttpClient.newBuilder().build();
+        HttpRequest httpRequest = HttpRequest.newBuilder()
+                .uri(URI.create("http://www.boredapi.com/api/actiity"))
+                .setHeader("Content-type", "application/json")
+                .build();
+
+        try {
+            httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        JSONParser jsonParser = new JSONParser();
+        try {
+            jsonObject = (JSONObject) jsonParser.parse(httpResponse.body());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    @DisplayName("URI Path")
+    public void testUriPath() {
+        Assertions.assertEquals("/api/actiity", httpResponse.uri().getPath());
+    }
+
+    @Test
+    @DisplayName("Full URI")
+    public void testFullUri() {
+        Assertions.assertEquals("http://www.boredapi.com/api/actiity", httpResponse.uri().toString());
+    }
+
+    @Test
+    @DisplayName("Status code is 200")
+    public void testStatusCode() {
+        Assertions.assertEquals(200L, httpResponse.statusCode());
+    }
+
+    @Test
+    @DisplayName("Error message")
+    public void testResult() {
+        Assertions.assertEquals("Endpoint not found", jsonObject.get("error"));
+    }
+}
